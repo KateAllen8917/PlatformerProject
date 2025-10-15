9b71b709788d696a82276a8cd8516ddc8758dcef
KateAllen8917 <768361@newcollege.ac.uk>
2025-10-15 10:26:27 +0100

    15/10/25 lesson
    section 1 level 1 in progress
    character controller can detect forward

---
+++ Assets/controler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controler : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Debug.Log("MAMA OOOOOOO")
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+
+        }
+    }
+}
